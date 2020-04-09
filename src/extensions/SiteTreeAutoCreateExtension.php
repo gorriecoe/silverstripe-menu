b838921943645a65cf5e507da79cdc39aea20909
Gorrie Coe <gorriecoe@gmail.com>
2020-04-09 15:57:41 +1200

    Added automatic delete to a menu after deleting a page

--- src/extensions/SiteTreeAutoCreateExtension.php
+++ src/extensions/SiteTreeAutoCreateExtension.php
@@ -2,9 +2,9 @@
 
 namespace gorriecoe\Menu\Extensions;
 
-use SilverStripe\Dev\Debug;
 use gorriecoe\Menu\Models\MenuSet;
 use gorriecoe\Menu\Models\MenuLink;
+use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\DataExtension;
 
@@ -18,6 +18,24 @@ use SilverStripe\ORM\DataExtension;
 class SiteTreeAutoCreateExtension extends DataExtension
 {
     /**
+     * Get list of menus owned by this page.
+     * @return ArrayList
+     */
+    public function getOwnsMenu()
+    {
+        $owner = $this->owner;
+        $owns = $owner->config()->get('owns_menu') ? : [];
+        $menuSets = ArrayList::create();
+
+        foreach ($owns as $key => $slug) {
+            if ($menuSet = MenuSet::get_by_slug($slug)) {
+                $menuSets->push($menuSet);
+            }
+        }
+        return $menuSets;
+    }
+
+    /**
      * Event handler called after writing to the database.
      */
     public function onAfterWrite()
@@ -25,24 +43,41 @@ class SiteTreeAutoCreateExtension extends DataExtension
         parent::onAfterWrite();
         $owner = $this->owner;
 
-        $autoCreateList = $owner->config()->get('owns_menu') ? : [];
-
-        foreach ($autoCreateList as $key => $slug) {
-            if ($menuSet = MenuSet::get_by_slug($slug)) {
-                $menuLink = DataObject::get_one(MenuLink::class, [
+        foreach ($this->getOwnsMenu() as $menuSet) {
+            $menuLink = DataObject::get_one(MenuLink::class, [
+                'Type' => 'SiteTree', // Ensures the editor hasn't intentionally changed this link.
+                'MenuSetID' => $menuSet->ID,
+                'SiteTreeID' => $owner->ID
+            ]);
+            if ($menuLink) {
+                $menuLink->setField('Title', $owner->MenuTitle);
+            } else {
+                $menuLink = MenuLink::create([
+                    'Type' => 'SiteTree',
                     'MenuSetID' => $menuSet->ID,
                     'SiteTreeID' => $owner->ID
                 ]);
-                if ($menuLink) {
-                    $menuLink->setField('Title', $owner->Title);
-                } else {
-                    $menuLink = MenuLink::create([
-                        'Type' => 'SiteTree',
-                        'MenuSetID' => $menuSet->ID,
-                        'SiteTreeID' => $owner->ID
-                    ]);
-                };
-                $menuLink->write();
+            };
+            $menuLink->write();
+        }
+    }
+
+    /**
+     * Event handler called before deleting from the database.
+     */
+    public function onBeforeDelete()
+    {
+        parent::onBeforeDelete();
+        $owner = $this->owner;
+
+        foreach ($this->getOwnsMenu() as $menuSet) {
+            $menuLink = DataObject::get_one(MenuLink::class, [
+                'Type' => 'SiteTree',
+                'MenuSetID' => $menuSet->ID,
+                'SiteTreeID' => $owner->ID
+            ]);
+            if ($menuLink) {
+                $menuLink->delete();
             }
         }
     }
